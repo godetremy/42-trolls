af6841231ff37931a1bc989aa6ef94f0e0656b17
Rémy Godet <77058107+godetremy@users.noreply.github.com>
2025-03-03 10:07:50 +0100

    adsh: Use of flatpak to run + handle ctrl+c

--- adsh/adsh.sh
+++ adsh/adsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# List of video in video folder
+# Video file in ~/.adsh/video
 VIDEO=(
     "2024_carrefour.mp4"
     "2024_croustibat.mp4"
@@ -9,8 +9,23 @@ VIDEO=(
     "2025_sfr.mp4"
 )
 
-# Selecy random video in the list
-AD="${VIDEO[$RANDOM % ${#VIDEO[@]}]}"
+# Get random video
+AD="${VIDEO[RANDOM % ${#VIDEO[@]}]}"
 
-# Play video with mpv
-mpv --vo=tct --vo-tct-256 --term-status-msg="\n\n\n\nYour command will be executed shortly" --msg-level=cplayer=no --osc=no --keep-open=no --display-tags=no ~/.adsh/video/$AD
+# Cleanup if ctrl+c
+cleanup() {
+    kill -SIGTERM "$MPV_PID" 2>/dev/null
+    wait "$MPV_PID" 2>/dev/null
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+# Play the ad every other execution (based on RANDOM % 2)
+if (( RANDOM % 2 == 0 )); then
+
+    flatpak run io.mpv.Mpv --vo=tct --vo-tct-256 --term-status-msg="\n\n\n\nVotre commande sera exécutée bientôt" --msg-level=cplayer=no --osc=no --keep-open=no --display-tags=no ~/.adsh/video/"$AD" &
+    MPV_PID=$!
+
+    wait "$MPV_PID"
+fi
